768d26ae99c631fa34e16b775635d9984e8134c2
rr <51890027+int-72h@users.noreply.github.com>
2020-08-24 22:56:56 +0000

    made ruby better

--- riesi.rb
+++ riesi.rb
@@ -1,6 +1,7 @@
 puts "Riesi: False"
 puts "Riesi?"
 inp = gets
-puts "Riesi: True" if inp == "y\n"
-puts "Riesi: False" if inp == "n\n"
-
+if inp == "y\n"
+puts "Riesi: True"
+else puts "Riesi: False"
+end
