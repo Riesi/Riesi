2d653613e919bd59e64e76b5d82f0bb2ec6fb335
rr <51890027+int-72h@users.noreply.github.com>
2020-08-24 22:27:50 +0000

    made riesi.c more compact

--- riesi.c
+++ riesi.c
@@ -1,25 +1,15 @@
 /* gcc -std=c89 riesi.c -o riesi_c -pedantic -Wall -Wextra -Werror */
 #include <stdio.h> /* getchar */
-#include <ctype.h> /* tolower */
 
 int main(void)
 {
-    int  riesi = 0;
-    char input;
-
     printf("Riesi: False\n");
-    printf("Riesi?");
-
-    input = getchar();
-    input = tolower(input);
-    
-    riesi = (input == 'y');
-
-    if (riesi == 0)
-        printf("Riesi: False\n");
-    else
+    printf("Riesi? ");
+    char riesi = getchar();
+    if (riesi == 'y')
         printf("Riesi: True\n");
-
+    else
+        printf("Riesi: False\n");
     return 0;
 }
 
